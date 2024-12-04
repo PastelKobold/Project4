8eee49fcd821570c667b1ced0e6fd497e10b08dd
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2024-12-04 15:31:01 -0500

    12/4

---
+++ poker.py
@@ -0,0 +1,16 @@
+from cards import *
+
+class Player :
+    hand = []
+
+    # def allOneColor(hand):
+        
+
+class Poker :
+    def __init__ (self, numPlayers) :
+        self.players = []
+        self.drawDeck = Deck(True)
+
+
+if __name__ == '__main__': 
+    game = Poker(4)
